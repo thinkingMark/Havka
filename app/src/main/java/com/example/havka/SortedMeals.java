167e46176acd1edfd8486c59df393094799c999d
thinkingMark <mark.rakochyi@gmail.com>
2020-03-18 21:15:25 +0200

    18.03.2020 update

--- app/src/main/java/com/example/havka/SortedMeals.java
+++ app/src/main/java/com/example/havka/SortedMeals.java
@@ -3,22 +3,35 @@ package com.example.havka;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SortedMeals extends AppCompatActivity {
+
+    ListView listView;
+    String mMealTitle[] = {"BORSHT","VARENYKY","UZVAR","SIRNIKS"};
+    String mMealDescription[] = {"Borsch is a sour soup common in Eastern Europe and Northern Asia.Made with beetroots as one of the main ingredients, which give the dish its distincti-ve red color.",
+            "Ukrainian dish made of boiled dough with diverse fillings, such as meat,vegetables, fruits, cheese etc. These Ukrainian dumplings can be the main course as well as the dessert.",
+            "The uzvar is a national Ukrainian beverage, cooked with dried fruits and berries.Some housewives tend to add species – star anise or nutmeg – that will give the drink an exotic flavor.",
+            "Fried Eastern Slavic quark pancakes, garnished with sour cream, varenye, jam, honey or apple sauce. The cheese mixture may contain raisins for extra flavour."};
+    String mMealPrice[] = {"10$","9$","16$","7$"};
+    String mMealTime[] = {"1 H","2 h","2.5h","0.5H"};
+    int mMealImages[] = {R.drawable.uzvar,R.drawable.varenyky,R.drawable.uzvar,R.drawable.sirniks};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sorted_meals);
 
-
+        listView = findViewById(R.id.listView);
 
         // Ініцілізуємо змінну та присвоюємо їй обєкт з activity_main.xml
         BottomNavigationView bottomNavigationView = findViewById(R.id.bootom_navigation);
@@ -58,6 +71,23 @@ public class SortedMeals extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context;
+        String rMealTitle;
+        String rMealDescription;
+        String rMealPrice;
+        String rMealTime;
+        int rMealImage;
+
+        MyAdapter(Context c, String title[], String description[], String price[], String time[],int images[]){
+            super(c,R.layout.m){
+
+            };
+
+        }
 
     }
 }
