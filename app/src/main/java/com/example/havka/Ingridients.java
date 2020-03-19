bb2b3888dd0fd5b55ed864007d2b494b4aece421
vlad-dvlad <vlad.ivashko72@gmail.com>
2020-03-19 14:17:55 +0200

    Sixth-page for Borsch

--- app/src/main/java/com/example/havka/Ingridients.java
+++ app/src/main/java/com/example/havka/Ingridients.java
@@ -2,14 +2,50 @@ package com.example.havka;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Ingridients extends AppCompatActivity {
+
+    private EditText liter;
+    private Button generate;
+
+    private int[] arrayOfProportion = { // інгдедієнти
+            100, 150, // sausage, beet
+            70, 200,  // carrot, potato,
+            50, 70,   // oil, onion
+            100, 700, // paste, water,
+            100, 100, // cabbage, tomato,
+            10, 30,   // garlic, pepper,
+            15, 30    // salt, parsley
+    };
+
+    /*
+    Створюємо змінні для списку продуктів
+     */
+
+    private TextView sausage;
+    private TextView beet;
+    private TextView carrot;
+    private TextView potato;
+    private TextView oil;
+    private TextView onion;
+    private TextView paste;
+    private TextView water;
+    private TextView cabbage;
+    private TextView tomato;
+    private TextView garlic;
+    private TextView pepper;
+    private TextView salt;
+    private TextView parsley;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +78,65 @@ public class Ingridients extends AppCompatActivity {
                 return false;
             }
         });
+        addListenerButton();
+    }
 
+    public void addListenerButton() {
+        liter = (EditText)findViewById(R.id.liter);
+        generate = (Button)findViewById(R.id.generateLiters);
+
+        /*
+        Додаємо продукти на панель, присвоюючи їм id
+         */
+
+        sausage = (TextView)findViewById(R.id.valueOfSousage);
+        beet = (TextView)findViewById(R.id.valueOfBeet);
+        carrot = (TextView)findViewById(R.id.valueOfCarrot);
+        potato = (TextView)findViewById(R.id.valueOfPotato);
+        oil = (TextView)findViewById(R.id.valueOfOil);
+        onion = (TextView)findViewById(R.id.valueOfOnion);
+        paste = (TextView)findViewById(R.id.valueOfPast);
+        water = (TextView)findViewById(R.id.valueOfWater);
+        cabbage = (TextView)findViewById(R.id.valueOfCabbage);
+        tomato = (TextView)findViewById(R.id.valueOfTomato);
+        garlic = (TextView)findViewById(R.id.valueOfGarlic);
+        pepper = (TextView)findViewById(R.id.valueOfPepper);
+        salt = (TextView)findViewById(R.id.valueOfSalt);
+        parsley = (TextView)findViewById(R.id.valueOfParsley);
+
+        generate.setOnClickListener(
+                new View.OnClickListener() {
+
+                    /*
+                    в наступному методі відбувається генерація продуктів, в якій обраховуються пропорції інгредієнтів
+                     */
+
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(
+                                Ingridients.this, liter.getText(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+
+                        int valueOfIngredients = Integer.parseInt(liter.getText().toString());
+                        int proportion = 20*(valueOfIngredients - 1);
+
+                        sausage.setText(new Integer(arrayOfProportion[0] + proportion).toString());
+                        beet.setText(new Integer(arrayOfProportion[1] + proportion).toString());
+                        carrot.setText(new Integer(arrayOfProportion[2] + proportion).toString());
+                        potato.setText(new Integer(arrayOfProportion[3] + proportion).toString());
+                        oil.setText(new Integer(arrayOfProportion[4] + proportion).toString());
+                        onion.setText(new Integer(arrayOfProportion[5] + proportion).toString());
+                        paste.setText(new Integer(arrayOfProportion[6] + proportion).toString());
+                        water.setText(new Integer(arrayOfProportion[7] + 900 * (valueOfIngredients - 1)).toString());
+                        cabbage.setText(new Integer(arrayOfProportion[8] + proportion).toString());
+                        tomato.setText(new Integer(arrayOfProportion[9] + proportion).toString());
+                        garlic.setText(new Integer(arrayOfProportion[10] + proportion / 2).toString());
+                        pepper.setText(new Integer(arrayOfProportion[11] + proportion / 4).toString());
+                        salt.setText(new Integer(arrayOfProportion[12] + proportion / 4).toString());
+                        parsley.setText(new Integer(arrayOfProportion[13] + proportion / 2).toString());
+                    }
+                }
+        );
     }
 }
